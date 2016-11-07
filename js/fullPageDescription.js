f4ca1d0c3bdc3431b5fdf5dd69e79b20aef4434f
Gleb Motuz <hihoho@mail.ru>
2016-11-07 14:44:39 +0200

    V1.1

---
+++ js/fullPageDescription.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      sectionsColor: ['', '', ''],
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+
+      menu: '#menu',
+      navigation: true
+    });
+});
